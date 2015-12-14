3ea98790de3e2040016e32b27265160d743cc75b
skmllns <skmllns@gmail.com>
2015-12-14 14:19:19 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -13,10 +13,7 @@ headers = ["id", "class", "enrollment", "gender", "word"]
 wb = openpyxl.load_workbook(read_file)
 sheet = wb.get_sheet_by_name('Sheet1')
 row_count = sheet.get_highest_row() - 1
-
-words = re.findall(r'\w+', sheet.cell(row=1, column=3).value.lower())
-tagged_words = nltk.pos_tag(words)
-      
+     
 with open(write_file, 'wb') as f: 
    writer = csv.writer(f)
    writer.writerow(headers)
@@ -24,11 +21,10 @@ with open(write_file, 'wb') as f:
       year = sheet.cell(row=row_num, column=0).value
       enrollment = sheet.cell(row=row_num, column=1).value
       gender = sheet.cell(row=row_num, column=2).value
-      words = re.findall(r'\w+', sheet.cell(row=row_num, column=3).value.lower())
+      words = re.findall(r'\w+\'*\w*', sheet.cell(row=row_num, column=3).value.lower())
       tagged_words = nltk.pos_tag(words)
       for word in tagged_words:
          if word[1] in pos_to_pull:
-            print row_num, year, enrollment, gender, word[0]
             writer.writerow([row_num, year, enrollment, gender, word[0]])
 
       
